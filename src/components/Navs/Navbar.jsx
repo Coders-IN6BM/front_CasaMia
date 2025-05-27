31d8ce160583b0348501ba2cc2f24dec23e3809c
Luxem <elux-2021572@kinal.edu.gt>
2025-05-26 20:09:33 -0600

    Fix: Navbar

--- src/components/Navs/Navbar.jsx
+++ src/components/Navs/Navbar.jsx
@@ -12,7 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
-const pages = ['REGISTER', 'HOTELES', 'USUARIOS'];
+const pages = ['REGISTER', 'HOTELES', 'EVENTOS'];
 const settings = ['Profile', 'Logout'];
 const casaMiaMainBlue = "#2563eb";
 
